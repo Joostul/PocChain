8b91cd9cc04859e664155ff826c3533097bcc3b4
Joost van Uitert <joostvanuitert@gmail.com>
2018-04-11 22:45:50 +0200

    Implement block

---
+++ PocChain/PocChain/PocBlock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PocChain
+{
+    public class PocBlock
+    {
+        private long Index { get; set; }
+        private DateTime TimeStamp { get; set; }
+        private object Data { get; set; }
+        private string PreviousHash { get; set; }
+        private string Hash { get; set; }
+
+        public PocBlock(long index, DateTime timeStamp, object data, string previousHash)
+        {
+            Index = index;
+            TimeStamp = timeStamp;
+            Data = data;
+            PreviousHash = previousHash;
+            Hash = CalculateHash();
+        }
+
+        private string CalculateHash()
+        {
+            var hashString = new SHA256Managed();
+            var input = Encoding.ASCII.GetBytes(Index + TimeStamp.ToString() + Data + PreviousHash);
+            var result = hashString.ComputeHash(input);
+            return result.ToString();
+        }
+    }
+}
